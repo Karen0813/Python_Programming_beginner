125ed18c135d363a99627ed16486a4b7ec1aa161
Karen0813 <Karen1210813@gmail.com>
2025-03-25 20:51:45 +0800

    Update ex2-1.py

--- ex2-1.py
+++ ex2-1.py
@@ -1,9 +1,25 @@
+# 在 Python 中，用「一個井字號 # 」做為單行註解，用「三個雙引號 """ 或三個單引號 '''」做多行註解
+
 # this is my first program
-print ('Hello World')
+
+print ("Hello World")
 
 # 可直接輸出
 print(2*3)
+print(2+9)
 
 # 亦可新增變數
 a1=9*8
 print(a1)
+
+b=0
+b=b+1
+print(b)
+
+'''
+從1+2+...+100
+'''
+c = 0  # 初始化變數c為0
+for i in range(1, 101):  
+    c += i  # 將每個數字加到c上
+print(c)  # 輸出
